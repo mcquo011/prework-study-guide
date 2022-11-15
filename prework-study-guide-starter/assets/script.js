ef0f8c697efc33c719e77e689e232bb66df6957e
mcquo011 <116676004+mcquo011@users.noreply.github.com>
2022-11-15 10:42:51 -0600

    Update script.js
    Changed randomTopic === 'Javascrip' to randomTopic === 'JavaScript'

--- prework-study-guide-starter/assets/script.js
+++ prework-study-guide-starter/assets/script.js
@@ -14,7 +14,7 @@ if (randomTopic === 'HTML'){
     console.log("Let's study CSS!");
 } else if (randomTopic === 'Git'){
     console.log("Let's study Git!");
-} else if(randomTopic === 'JavaScrip'){
+} else if(randomTopic === 'JavaScript'){
     console.log("Let's study JavaScript!");
 } else {
     console.log('Please try again!');
